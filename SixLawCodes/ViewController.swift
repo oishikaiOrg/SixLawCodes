f5789497359c11a3e0a8356b0acbb8d960ee510d
Kai Oishi <bokuchan.kai@gmail.com>
2021-02-01 19:58:05 +0900

    基となる六つの法の選択画面を追加

--- SixLawCodes/ViewController.swift
+++ SixLawCodes/ViewController.swift
@@ -7,7 +7,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let sixCodes = ["憲法", "刑法", "民法", "商法", "刑事訴訟法", "民事訴訟法"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sixCodes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "code", for: indexPath)
+        cell.textLabel!.text = sixCodes[indexPath.row]
+        return cell
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
