0ef383824e273a077a23eec042c1c57f447de3b7
Kai Oishi <bokuchan.kai@gmail.com>
2021-02-10 00:27:14 +0900

    遷移元で選ばれたcellに対応した法令APIのurlと通信するコードを追加した

--- SixLawCodes/ViewController.swift
+++ SixLawCodes/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     let sixCodes = ["憲法", "刑法", "民法", "商法", "刑事訴訟法", "民事訴訟法"]
+    let lawNumber = ["昭和二十一年憲法","明治四十年法律第四十五号", "明治二十九年法律第八十九号", "明治三十二年法律第四十八号", "昭和二十三年法律第百三十一号", "昭和二十三年法律第百三十一号"]
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return sixCodes.count
@@ -25,7 +26,21 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         let storyboard = UIStoryboard(name: "Article", bundle: nil)  //storyboardクラスのインスタンスとしてArticle.storyboardを指定
         let vc = storyboard.instantiateViewController(identifier: "article")  //Article.storyboard中の"article"をIDとして持つviewを指定
         self.navigationController?.pushViewController(vc, animated: true)
-        //    let storyboard: UIStoryboard = self.storyboard!
+        print(indexPath.row) // 引数のidexpathを数値として受け取る
+        let setLawNumber = lawNumber[indexPath.row]
+        
+        let config = URLSessionConfiguration.default
+        let session = URLSession(configuration: config)
+        
+        print("https://elaws.e-gov.go.jp/api/1/lawdata/\(setLawNumber)")
+        // urlにlawNumberを埋め込んで,そのままだと扱えないので .addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlQueryAllowed)! でエンコード
+        
+        let url = URL(string: "https://elaws.e-gov.go.jp/api/1/lawdata/\(setLawNumber)".addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlQueryAllowed)!)!
+
+        let task = session.dataTask(with: url) { (data: Data?, response: URLResponse?, error: Error?) in
+            print(response)
+        }
+        task.resume()
     }
 
     override func viewDidLoad() {
