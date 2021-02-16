5770e07698e2b7da430982cece97c5c1ec4b1db5
Kai Oishi <bokuchan.kai@gmail.com>
2021-02-16 22:49:04 +0900

    取得した文章を全て表示するカスタムセルを追加した

--- SixLawCodes/ParagraphViewController.swift
+++ SixLawCodes/ParagraphViewController.swift
@@ -11,21 +11,35 @@ class ParagraphViewController: UIViewController, UITableViewDelegate, UITableVie
     var sentence: [String] = []
     var paragraphNum = 0
     
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return sentence.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "paragraph", for: indexPath)
-        cell.textLabel!.text = sentence[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: SentenceListTableViewCell.cellIdentifier, for: indexPath) as! SentenceListTableViewCell
+//        cell.textLabel!.text = sentence[indexPath.row]
+        cell.label.text = sentence[indexPath.row]
         return cell
     }
     
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
     
-    
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(sentence[0])
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = UITableView.automaticDimension
+        
+        let nib = UINib(nibName: SentenceListTableViewCell.cellIdentifier, bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: SentenceListTableViewCell.cellIdentifier)
     }
     
     
